729630870d0475c7728a7405a0082ed11df72240
melqui <elmenk2018@gmail.com>
2024-10-04 17:07:00 -0600

    update

--- JDBC/Universidad Java/introduccionJDBC/src/main/java/Testmenk/TestMysqlJDBC.java
+++ JDBC/Universidad Java/introduccionJDBC/src/main/java/Testmenk/TestMysqlJDBC.java
@@ -10,14 +10,21 @@ import java.util.logging.Logger;
 
 public class TestMysqlJDBC { //psvm=atajo de escritura de main
     public static void main(String[] args) {
+        // La ruta de la BD
         String url = "jdbc:mysql://localhost:3306/testmenk?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
         try {
+            //colocamos nombre de la clase de mysql - 
             Class.forName("com.mysql.cj.jdbc.Driver");
+            // propiedad tipo Connection
             Connection conexion = DriverManager.getConnection(url,"root","Admin");
+            // crear objeto statement, permite ejecutar sentencias sobre la tabla de BD
             Statement instruccion = conexion.createStatement();
             var sql = "SELECT id_persona, nombre, apellido, email, telefono FROM persona";
+            //ejecutar instrucción, utilizar el tipo interface ResultSet
             ResultSet resultado = instruccion.executeQuery(sql);
+            // iterar los posibles resultados dentro de ResultSEt (cada registro en la BD)
             while (resultado.next()){
+                //solo print sin salto de línea
                 System.out.print("Id Persona "+resultado.getInt("id_persona"));
                 System.out.print(" Nombre: "+resultado.getString("nombre"));
                 System.out.print(" Apellido: "+resultado.getString("apellido"));
@@ -25,6 +32,8 @@ public class TestMysqlJDBC { //psvm=atajo de escritura de main
                 System.out.print(" Telefono: "+resultado.getString("telefono"));
                 System.out.println("");
             }
+
+            // Cerrar los flujos de manera inversa, de cada objeto 
             resultado.close();
             instruccion.close();
             conexion.close();
@@ -32,6 +41,7 @@ public class TestMysqlJDBC { //psvm=atajo de escritura de main
             
             ex.printStackTrace(System.out);
         } catch (SQLException ex) {
+            System.out.println("Error en la conexión de la BD");
             Logger.getLogger(TestMysqlJDBC.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
